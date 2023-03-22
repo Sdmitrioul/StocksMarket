c0a637be23cd82c73e755d78d1d35cf09a9e9c12
”Dmitri <dmitri.zd1958@gmail.com>
2023-03-22 17:45:55 +0100

    Init commit

---
+++ market/src/main/java/ru.skroba/Market.java
@@ -0,0 +1,7 @@
+package ru.skroba;
+
+public class Market {
+    public static void main(String[] args) {
+        System.out.println("Hi");
+    }
+}
---
+++ user/src/main/java/ru.skroba/User.java
@@ -0,0 +1,7 @@
+package ru.skroba;
+
+public class User {
+    public static void main(String[] args) {
+        System.out.println("hi");
+    }
+}
