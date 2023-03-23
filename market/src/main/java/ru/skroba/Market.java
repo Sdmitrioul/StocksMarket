8b64ad1ab54467ac6401f3dbde453f3b25081a62
”Dmitri <dmitri.zd1958@gmail.com>
2023-03-23 23:54:44 +0100

    Add server to market

--- market/src/main/java/ru/skroba/Market.java
+++ market/src/main/java/ru/skroba/Market.java
@@ -1,11 +1,20 @@
 package ru.skroba;
 
+import io.reactivex.netty.protocol.http.server.HttpServer;
+import ru.skroba.configuration.MarketConfiguration;
+import ru.skroba.controllers.Controller;
+
 public class Market {
     public static void main(String[] args) {
-        System.out.println("Hi");
+        MarketConfiguration configuration = new MarketConfiguration();
+        
+        new Market().run(configuration);
     }
     
-    public void run() {
-    
+    public void run(MarketConfiguration configuration) {
+        Controller controller = configuration.getController();
+        HttpServer.newServer(configuration.getServerPort())
+                .start(((request, response) -> response.writeString(controller.processRequest(request))))
+                .awaitShutdown();
     }
 }
---
+++ market/src/main/java/ru/skroba/configuration/MarketConfiguration.java
@@ -0,0 +1,67 @@
+package ru.skroba.configuration;
+
+import ru.skroba.controllers.Controller;
+import ru.skroba.controllers.MasterController;
+import ru.skroba.controllers.stocks.BuyStocksController;
+import ru.skroba.controllers.stocks.CreateCompanyController;
+import ru.skroba.controllers.stocks.GetAllStocksController;
+import ru.skroba.controllers.stocks.SellStocksController;
+import ru.skroba.controllers.stocks.UpdateCompanyStocksController;
+import ru.skroba.repository.Database;
+import ru.skroba.repository.MongoStocksRepository;
+import ru.skroba.repository.StocksRepository;
+import ru.skroba.service.CompanyStocksService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MarketConfiguration {
+    private static final String MARKET_PORT = "MARKET_PORT";
+    private static final String DB_PORT = "MARKET_DB_PORT";
+    private static final String DB_NAME = "MARKET_DB_NAME";
+    
+    private Database database;
+    private StocksRepository repository;
+    
+    private CompanyStocksService stocksService;
+    
+    public int getServerPort() {
+        return Integer.parseInt(System.getenv(MARKET_PORT));
+    }
+    
+    public Controller getController() {
+        List<Controller> controllers = new ArrayList<>();
+        
+        controllers.add(new BuyStocksController(getStocksService()));
+        controllers.add(new CreateCompanyController(getStocksService()));
+        controllers.add(new GetAllStocksController(getStocksService()));
+        controllers.add(new SellStocksController(getStocksService()));
+        controllers.add(new UpdateCompanyStocksController(getStocksService()));
+        
+        return new MasterController(controllers);
+    }
+    
+    public CompanyStocksService getStocksService() {
+        if (stocksService == null) {
+            stocksService = new CompanyStocksService(getStockRepository());
+        }
+        
+        return stocksService;
+    }
+    
+    public StocksRepository getStockRepository() {
+        if (repository == null) {
+            repository = new MongoStocksRepository(getDatabase());
+        }
+        
+        return repository;
+    }
+    
+    public Database getDatabase() {
+        if (database == null) {
+            database = new Database(Integer.parseInt(System.getenv(DB_PORT)), System.getenv(DB_NAME));
+        }
+        
+        return database;
+    }
+}
